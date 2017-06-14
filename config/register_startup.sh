4b01e554ec9cd4afad927d5854c64703888dc002
Shuieryin <shuieryin@gmail.com>
2017-06-14 19:22:14 +0800

    added register startup

---
+++ config/register_startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PWD=$(pwd)
+SERVICE_PATH=/etc/systemd/system/run_command.service
+
+rm -f ${SERVICE_PATH}
+echo "[Unit]" | tee ${SERVICE_PATH}
+echo "After=docker.service" | tee -a ${SERVICE_PATH}
+echo "[Service]" | tee -a ${SERVICE_PATH}
+echo "ExecStart='${SERVICE_PATH}/_build/default/rel/run_command/bin/run_command start'" | tee -a ${SERVICE_PATH}
+echo "[Install]" | tee -a ${SERVICE_PATH}
+echo "WantedBy=default.target" | tee -a ${SERVICE_PATH}
+
+chmod 644 ${SERVICE_PATH}
+systemctl daemon-reload
+systemctl enable run_command.service
